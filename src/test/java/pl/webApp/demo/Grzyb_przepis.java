e86143c9e7449b42025da33a652caf24ce97e0c0
Agata <a.e.marteen@gmail.com>
2025-05-07 18:48:28 +0200

    poprawka poprzedniego commita: dodanie getterów do grzyb_przepis

--- src/test/java/pl/webApp/demo/Grzyb_przepis.java
+++ src/test/java/pl/webApp/demo/Grzyb_przepis.java
@@ -10,4 +10,12 @@ import lombok.NoArgsConstructor;
 public class Grzyb_przepis {
     private int id_grzyb;
     private int id_przepis;
+
+    public int getId_grzyb() {
+        return id_grzyb;
+    }
+
+    public int getId_przepis() {
+        return id_przepis;
+    }
 }
